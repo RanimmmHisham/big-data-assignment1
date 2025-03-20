6da18059fa2df494ae1f2b270c45293ccdb97107
Ranim Hisham <155158165+RanimmmHisham@users.noreply.github.com>
2025-03-20 19:20:54 +0200

    Delete assignment directory

--- assignment/load.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sys
-
-file_path = sys.argv[1]
-df = pd.read_csv(file_path)
-df.to_csv("loaded_data.csv", index=False)
-print("Data loaded successfully.")
-
-exec(open("dpre.py").read())
